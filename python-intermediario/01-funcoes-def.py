4ffdfcf1e7b73fa4edda426cf6371d262b4ff1dd
Joshuel <nobrej1999@gmail.com>
2022-01-20 15:32:10 -0300

    starting function

---
+++ python-intermediario/01-funcoes-def.py
@@ -0,0 +1,7 @@
+def saudacao(msg='Olá', nome='Usuário'):
+    nome = nome.replace('a','4')
+    print(msg, nome)
+
+saudacao(nome='Zé do gás', msg='Hello')
+saudacao('Oi','Carlinhos')
+saudacao('Olá','Joseph')
